47abc35c2b45370f286c977e0eda06b7335e90be
Efrat Friedman <efratfriedman18@gmail.com>
2025-10-22 13:22:07 +0300

    Refactor layout structure and styles in page and globals CSS

--- src/app/form/page.tsx
+++ src/app/form/page.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import SignUpForm from "../components/SignUp/Signup";
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
-import styles from "@/app/page.module.css";
 
 export default function Form() {
   return (
-   <div className={styles.pageContainer}>
+   <div className="page-container">
     <Header/>
+    <main className="content">
     <SignUpForm/>
+    </main>
     <Footer/>
    </div>
   );
